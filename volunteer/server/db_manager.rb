123be81ff0e117df572e86072e8e42b4691020c3
WoodyYo <lturtsamuel@gmail.com>
2015-01-02 04:52:14 +0800

    haml 初體驗

--- volunteer/server/db_manager.rb
+++ volunteer/server/db_manager.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 require 'sequel'
 
+LIST = ["happy", "angry", "sad", "joyful"]
+
 class DBManager
 	def initialize
 		@db = Sequel.sqlite('v_server.db')
@@ -17,6 +19,15 @@ class DBManager
 			DateTime :time
 			FalseClass :read, :default => false
 		end
+		@db.create_table? :sentences do
+			primary_key :id
+			String :sentence, :unique => true
+			Integer :emo
+			Integer :count, :default => 0
+		end
+		@users = @db[:users]
+		@messages = @db[:messages]
+		@sentences = @db[:sentences]
 		create_user "woodyyo5566" unless get_by_id "woodyyo5566"  # create super user
 	end
 	def create_user id
@@ -24,7 +35,7 @@ class DBManager
 			raise ArgumentError, 'ID Illegal'
 		end
 		begin
-			@db[:users].insert(:id => id)
+			@users.insert(:id => id)
 			{:id => id, :angry => 0, :sad => 0, :joyful => 0, :happy => 0, :msgs => []}
 		rescue => e
 			puts e
@@ -32,45 +43,62 @@ class DBManager
 		end
 	end
 	def get_by_id userid
-		user = @db[:users].where(:id => userid).first
+		user = @users.where(:id => userid).first
 		user[:msgs] = get_all_msgs(userid) if user
 		user
 	end
 	def puts_all_users
-		puts @db[:users].all
+		puts @users.all
 	end
 	def send_msg userid, msg
 		if get_by_id userid
-			@db[:messages].insert(:id => userid, :body => msg, :time => Time.now)
+			@messages.insert(:id => userid, :body => msg, :time => Time.now)
 			true
 		else
 			false
 		end
 	end
 	def get_all_msgs userid  # only return msgs' body. Setting the flag or not is the server's business!
-		msgs = @db[:messages].select(:body).where(:id => userid, :read => false).all
+		msgs = @messages.select(:body).where(:id => userid, :read => false).all
 		msgs.collect { |e| e[:body] }
 	end
 	def read_all_msgs userid  # only set the flag to true
-		@db[:messages].where(:id => userid, :read => false).update(:read => true)
+		@messages.where(:id => userid, :read => false).update(:read => true)
 	end
 	def set_emo_count userid, emo, count
-		@db[:user].where(:id => userid).update(emo => count)
+		@users.where(:id => userid).update(emo => count)
+	end
+	def get_sentence emo
+		emo = LIST.find_index emo
+		s = @sentences.where('count < 3 and emo=?', emo).first
+		@sentences.where(:id => s[:id]).update(:count => s[:count]+1)
+		s ? s[:sentence] : ''
+	end
+	def get_sentences emo
+		emo = LIST.find_index emo
+		@sentences.select(:sentence).where('count < 3 and emo=?', emo).collect do |e|
+			e[:sentence]
+		end
+	end
+	def add_sentence s, emo
+		emo = LIST.find_index emo
+		@sentences.insert(:sentence => s, :emo => emo)
 	end
 	#### ROOT's POWER ####
 	def dropall
 		puts "If u r sure to do this, please enter the project's name"
 		s = STDIN.readline.split("\n")[0]
 		if s == "Dj. Right"
-			puts "Got it~"
+			puts "Got it, master~"
 			@db.drop_table :users
 			@db.drop_table :messages
+			@db.drop_table :sentences
 		else
 			puts "Screw u"
 		end
 	end
 	def puts_all_msgs userid
-		@db[:messages].where(:id => userid).each { |e| puts e }
+		@messages.where(:id => userid).each { |e| puts e }
 	end
 	def DBManager.enter_msgs
 		s = ''
@@ -87,9 +115,12 @@ class DBManager
 		# auto terminate~
 	end
 	def broadcast msg
-		@db[:users].select(:id).each do |e|
+		@users.select(:id).each do |e|
 			send_msg e[:id], msg
 		end
+	end
+	def puts_all_sentences
+		@sentences.each {|e| puts e}
 	end
 end
 
@@ -104,6 +135,10 @@ if __FILE__ == $PROGRAM_NAME
 		m.puts_all_msgs ARGV[1]
 	elsif arg == 'broadcast'
 		DBManager.enter_msgs{|s| m.broadcast s}
+	elsif arg == 'sentences'
+		m.puts_all_sentences
+	elsif arg == 'adds'
+		DBManager.enter_msgs{|s| m.add_sentence s, ARGV[1]}
 	else
 		m.puts_all_users
 	end
--- volunteer/server/main.rb
+++ volunteer/server/main.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require 'sinatra'
 require 'json'
+require 'haml'
 load 'db_manager.rb'
 
 set :port, 8888
@@ -28,18 +29,35 @@ get '/profile/:id' do
 	else
 		"user #{id} not found!"
 	end
-	post '/upload/:id' do
-		userid = params[:id]
-		emo = params[:emo].to_sym  # A Symbol
-		user = Manager.get_by_id(userid)
-		ext = params[:file][:filename].split(".")[1]
-		begin
-			File.open([DataPath, emo, userid+"_"+user[emo]+ext].join "/", "w") do |f|
-				f.write(params[:file][:tempfile].read)
-			end
-			Manager.set_emo_count userid, emo, user[emo]
-			"OK"
-		rescue
-			"OOOPS"
+end
+get '/sentence/:emo' do # 拿單一句子，也就是手機端做的事
+	emo = params[:emo]
+	Manager.get_sentence emo # return '' if no sentence
+end
+get '/sentences/:emo' do # web version, 拿所有句子，附贈上傳表格
+	@emo = params[:emo]
+	@a = Manager.get_sentences @emo
+	haml :"sentences"
+end
+post '/sentences/:emo' do
+	@emo = params[:emo]
+	s = params[:sentence]
+	Manager.add_sentence s, @emo
+	@a = Manager.get_sentences @emo
+	haml :"sentences"
+end
+post '/upload/:id' do
+	userid = params[:id]
+	emo = params[:emo].to_sym  # A Symbol
+	user = Manager.get_by_id(userid)
+	ext = params[:file][:filename].split(".")[1]
+	begin
+		File.open([DataPath, emo, userid+"_"+user[emo]+ext].join "/", "w") do |f|
+			f.write(params[:file][:tempfile].read)
 		end
+		Manager.set_emo_count userid, emo, user[emo]
+		"OK"
+	rescue
+		"OOOPS"
 	end
+end
